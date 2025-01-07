6f32e0acdc948dd0c208a32c48a704cf2115a091
Siddharth-Tajane <tajanesiddharthsunil@gmail.com>
2025-01-07 07:02:57 +0000

    conversion of datatype

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,33 @@
+/*let score = "sid"
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//to convert the given data into the given format
+
+// "33" => 33
+//"33abc" => conersion will show nan but its type is number
+//true/false => 1/0
+*/
+
+
+let isLoggedIn = "sidd"
+
+
+let booleanIsLogedIn = Boolean(isLoggedIn)
+console.log(booleanIsLogedIn)
+
+
+
+// 1 will covert in true and 0 will convert in false in boolean conversion
+//""  empty string will show false
+//"sid" will show true
+
+
+
+//coverting number to string
+
+let no = 22
+let StringNumber = String(no)
+console.log(StringNumber)
+console.log(typeof StringNumber)
