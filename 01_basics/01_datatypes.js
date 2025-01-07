11a5e38c9d2e2a0a3abe6c6103f9493117982da5
Siddharth-Tajane <tajanesiddharthsunil@gmail.com>
2025-01-07 06:10:48 +0000

    datatypes of js

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";  //use all js code as newer version    and not need to use it is automatically executed
+
+//  alert( 3 + 3)    //will show error because we are using node and it is not supported
+
+let name = "sidd" 'sidd' //string datatype
+let age = 3 // numeric datatype
+let isLoggedIn = false  //boolean  true or false value
+
+
+
+// null => stand alone value     
+// undefined => value not defined  eg let state;
+// symbol => to show uniqueness
+
+
+
+
+
+// object
+
+/*   1. typeof  => used to know te typ of variable
+     eg   console.log(typeof undefined)
+     o/p   undefined
+     
+     eg   console.log(typeof null)
+     o/p   object
