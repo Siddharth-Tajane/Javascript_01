d25ba40b95c6963a6390e460005838339e585958
Siddharth-Tajane <tajanesiddharthsunil@gmail.com>
2025-01-07 08:08:28 +0000

    datatype summary

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -2,9 +2,9 @@
 
 //  alert( 3 + 3)    //will show error because we are using node and it is not supported
 
-let name = "sidd" 'sidd' //string datatype
-let age = 3 // numeric datatype
-let isLoggedIn = false  //boolean  true or false value
+// let name = "sidd" 'sidd' //string datatype
+// let age = 3 // numeric datatype
+// let isLoggedIn = false  //boolean  true or false value
 
 
 
@@ -24,3 +24,29 @@ let isLoggedIn = false  //boolean  true or false value
      
      eg   console.log(typeof null)
      o/p   object
+
+
+
+
+
+/* primitive data types
+
+
+   //7 types : string, Number, Boolean, Null, Undefined, Symbol, Bigint*/
+
+    const id = symbol('213') //eg of symbol
+
+// non primitive 
+
+    // array
+    // ["  "]
+    // 
+    // Objects
+    // {key : "value"}
+    // 
+    //  Functions
+    const myFunction = function(){
+     console.log("hello world");
+     
+    }
+
