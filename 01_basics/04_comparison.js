d23513f86986efe6f3a43c5d07f1c6d8703ab810
Siddharth-Tajane <tajanesiddharthsunil@gmail.com>
2025-01-07 07:51:01 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+// you should compare within only similar datatypes
+//we should avoid these type of comparison
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null < 0);
+// console.log(null == 0);
+
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+// console.log(undefined < 0);
+
+console.log("2" === 2);
+
+
+
+
+
