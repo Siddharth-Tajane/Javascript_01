c787b02358f360ba54eeb3e009647fd22ec2147c
Siddharth-Tajane <tajanesiddharthsunil@gmail.com>
2025-01-07 07:41:50 +0000

    operations

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -29,5 +29,32 @@ console.log(booleanIsLogedIn)
 
 let no = 22
 let StringNumber = String(no)
-console.log(StringNumber)
-console.log(typeof StringNumber)
+// console.log(StringNumber)
+// console.log(typeof StringNumber)
+
+//******************operation*******************//
+
+let value = 5
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2 + 2);
+console.log("2" + 2);
+console.log(2 + "2");
+console.log(1 + 2 + "2");
+console.log("1" + 2 + 2);
+
+console.log(+true);
+console.log(+false);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
