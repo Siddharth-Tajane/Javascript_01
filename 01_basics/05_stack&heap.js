5b6872402f5fe90d39ba7eac9496bedd76336af3
Siddharth-Tajane <tajanesiddharthsunil@gmail.com>
2025-01-09 08:00:42 +0000

    stack and heap

---
+++ 01_basics/05_stack&heap.js
@@ -0,0 +1,29 @@
+// //stack is used for primitive datatype
+// //hep is used for non primitive datatype
+
+
+// //stack
+
+// let myname = "sidd"
+
+// let anothername = myname
+// anothername = "siddh"
+
+// console.log(myname);
+// console.log(anothername);
+
+
+
+let userone = 
+{
+    email
+}
+
+let usertwo = userone
+
+usertwo.email = "Sidd@gmail.com"
+
+console.log(userone);
+console.log(usertwo);
+
+
