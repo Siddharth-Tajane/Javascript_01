2e08a25cc71ea4f5bff7262e7a5ff2f4e53b1cba
Siddharth-Tajane <tajanesiddharthsunil@gmail.com>
2025-01-11 14:58:32 +0000

    while and do-while

---
+++ 04_iterators/17_while.js
@@ -0,0 +1,21 @@
+let i = 0
+while (i <= 10) {
+    console.log(`value of i is ${i}`);
+    i = i + 2
+}
+
+
+const myarray = ["flash", "batman", "superman"]
+let arr = 0
+while (arr < myarray.length) {
+    console.log(`value is ${myarray[arr]}`);
+    arr = arr + 1
+}
+
+
+// do while loop
+let score = 1
+do {
+    console.log(`value of score is ${score}`);
+    score  ++
+} while (score <= 10);
